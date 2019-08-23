6ad66d496e5a1d1740cbffe00cdc4e3de8e3d75f
pyson <zxf509@163.com>
2019-08-23 12:36:34 +0800

    user phone

--- src/main/java/com/jeesite/modules/home/HomeController.java
+++ src/main/java/com/jeesite/modules/home/HomeController.java
@@ -3,16 +3,34 @@
  */
 package com.jeesite.modules.home;
 
+import com.jeesite.common.codec.DesUtils;
+import com.jeesite.common.config.Global;
+import com.jeesite.common.lang.StringUtils;
+import com.jeesite.common.shiro.filter.FormAuthenticationFilter;
+import com.jeesite.common.shiro.realm.BaseAuthorizingRealm;
+import com.jeesite.common.shiro.realm.LoginInfo;
 import com.jeesite.common.web.BaseController;
+import com.jeesite.common.web.CookieUtils;
+import com.jeesite.common.web.http.ServletUtils;
+import com.jeesite.modules.sys.utils.UserUtils;
+import org.apache.shiro.authz.UnauthorizedException;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.web.util.WebUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 
 @Controller
 public class HomeController extends BaseController {
 
 	@RequestMapping(value = "/home")
-	public String home() {
+	public String home(HttpServletRequest request, HttpServletResponse response, Model model) {
 		System.out.println(">>>>>>>>>>>>>>>>>HomePage<<<<<<<<<<<<<<<<");
 		return "home/index";
 	}
